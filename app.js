bcf4fc5b61b4b34f473ae75fba952393384fb896
amitsharma-hs01 <amitsharma.hs01@gmail.com>
2023-10-10 18:37:36 +0530

    test 1

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const __dirname=path.dirname(__filename)
 //middleware
 app.use(cors());
 app.use(bodyParser.json())
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,"./drip/build")))
 
 //routes
