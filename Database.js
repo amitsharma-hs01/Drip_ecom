abe254a459e3e094ac0c8641f38db5a55ef98a1b
amitsharma-hs01 <amitsharma.hs01@gmail.com>
2023-10-10 20:01:53 +0530

    test 4

--- Database.js
+++ Database.js
@@ -4,7 +4,7 @@ dotenv.config()
 import mongoose from "mongoose";
 
 export const  connectDb=async ()=>{
-   await mongoose.connect(process.env.DB_URI).then(()=>{
+   await mongoose.connect("mongodb+srv://amit:amit9898@ecom.rxb9zsk.mongodb.net/?retryWrites=true&w=majority").then(()=>{
         console.log("connected to the database")
     }).catch((err)=>{console.log(err)})
 }
