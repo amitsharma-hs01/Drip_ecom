db448d4cde565087cd0595eab02295ac9d80cf3f
amitsharma-hs01 <amitsharma.hs01@gmail.com>
2023-10-12 13:08:28 +0530

    continue

--- app.js
+++ app.js
@@ -7,21 +7,13 @@ import bodyParser from "body-parser"
 import cors from "cors";
 import categoryRouter from "./routes/categoryRoutes.js";
 import productRouter from "./routes/productRoutes.js";
-import path from "path"
-import {fileURLToPath} from "url"
+
 
 const app=express();
 
-
-//
-
-const __filename=fileURLToPath(import.meta.url)
-const __dirname=path.dirname(__filename)
 //middleware
 app.use(cors());
 app.use(bodyParser.json())
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,"./drip/build")))
 
 //routes
 app.use("/api/v1/auth",authRouter);
@@ -38,8 +30,3 @@ const port = process.env.PORT || 5000
         console.log(`server is running on port ${port}`)
     })
 }); 
-
-//rest api
-app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./drip/build/index.html'))
-})
